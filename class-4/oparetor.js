5c05b62af8eed6e4e2c88a57c44587a3fe95f30d
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-20 02:11:03 +0600

    oparetor its ok

--- class-4/oparetor.js
+++ class-4/oparetor.js
@@ -2,13 +2,19 @@
 
 // 1. Arithmetics Operators 
 (Addition, Subtraction, Multiplication, Exponentiation, Division, Modulus(Division Remainder), Increment(pre Increment, Post Increment), Decrement(pre Decrement, post decrement))
-// End of ArithMetics Operator 
+    // End of ArithMetics Operator 
 
 
-2. Assignment Operator
-3. String Operator
-4. Comparison   Operator
-5. conditional Operator
-6. Logical Operator
-7. BitWise Operator
+    // 2. Assignment Operator
+    (=, +=, *, / , % , *=, **=, / =, %=, ***= etc)
+    // 3. String Operator
+    (Same);
+// 4. Comparison   Operator
+(>, < , >=, <= , == , === , != , !== , >== , <== etc);
+// 5. conditional Operator
+(if, else, else -if, switch , ternary);
+// 6. Logical Operator
+     (&& (LogiCal and), || (Logical Or) );
+// 7. BitWise Operator
+()
 

a20892531a418e7fb5b6ca57167d5d0d78a46107
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-20 02:03:17 +0600

    first commit

---
+++ class-4/class-4.js
@@ -0,0 +1,17 @@
+// Number System 
+
+
+/* 1. Decimal = (0,1,2,3,4,5,6,7,8,9) ---- 10
+2. Binary = (0.1) --- 2
+3. octal = (0,1,2,3,4,5,6,7) ---- 8 
+4. Hexa-decimal = (0,1,2,3,4,,5,6,7,8,9,10(A), 11(B), 12(C), 13(D), 14(E), 15(F)); */
+
+const deci = 567;
+const binary = 10101;
+const octal = 045;
+const hexaDecimal = 0x435D;
+
+console.log(deci);
+console.log(binary);
+console.log(octal);
+console.log(hexaDecimal);
---
+++ class-4/oparetor.js
@@ -0,0 +1,14 @@
+// operator JS
+
+// 1. Arithmetics Operators 
+(Addition, Subtraction, Multiplication, Exponentiation, Division, Modulus(Division Remainder), Increment(pre Increment, Post Increment), Decrement(pre Decrement, post decrement))
+// End of ArithMetics Operator 
+
+
+2. Assignment Operator
+3. String Operator
+4. Comparison   Operator
+5. conditional Operator
+6. Logical Operator
+7. BitWise Operator
+

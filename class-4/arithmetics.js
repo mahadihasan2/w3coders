c467eee6a778b14aa7411ecea7068c9fe6ed132f
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-20 02:25:02 +0600

    arithmetics is done

---
+++ class-4/arithmetics.js
@@ -0,0 +1,14 @@
+let x = 20;
+const y = 30;
+const z = x + y;
+const w = x - y;
+const q = x * y;
+const d = x / y;
+const e = x++;
+
+console.log("Addition Number is :", z);
+console.log("Subtraction Number is : ", w);
+console.log("Multipication Number is : ", q);
+console.log("Division Number is: ", d);
+console.log("this Number is :", e);
+console.log("number is ", e);

2cd1af6d01bff84c39ac71fe127efd77af6ead0e
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-23 13:05:44 +0600

    update assignment oparetor

--- class-4/assignment.js
+++ class-4/assignment.js
@@ -8,14 +8,24 @@ const f = a -= b;
 const z = a / b;
 const x = a % b;
 const y = a * b;
-const t = a * a;
+const t = a **= b
+const u = a == b // Just data compare kore;
+
+let num1 = "10";
+let num2 = "10";
+// const number = num1 === num2;
+
+
 
 // console.log(c);
 console.log("This value of f = ", f);
 console.log("This value of z = ", z);
 console.log("This value of x = ", x);
 console.log("This value of y = ", y);
-console.log("This value of t = ", t)
+console.log("This value of t = ", t);
+console.log("this value of u = ", u);
+console.log(num1 === num2);
+console.log(num1 !== num2);
 
 
 // Assignment operator  just equal equal 

bc4a358266d275481e784e00029cceeb96ae6ed2
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-23 12:01:07 +0600

    operator is done

---
+++ class-4/assignment.js
@@ -0,0 +1,34 @@
+
+// AssignMent Operator logicalOR and equal
+let a = 50;
+let b = 40;
+// console.log(a =+ b);
+// const c = a += b;
+const f = a -= b;
+const z = a / b;
+const x = a % b;
+const y = a * b;
+const t = a * a;
+
+// console.log(c);
+console.log("This value of f = ", f);
+console.log("This value of z = ", z);
+console.log("This value of x = ", x);
+console.log("This value of y = ", y);
+console.log("This value of t = ", t)
+
+
+// Assignment operator  just equal equal 
+
+let d = 30;
+let e = 40;
+// const g = d = e ;
+// console.log()
+if (d == e) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+
+
---
+++ class-4/comparision.js
@@ -0,0 +1,14 @@
+
+// Comparison and Conditional operator and Logical operator
+
+let a = 120;
+let b = 160;
+let c = 10;
+if (a > b && a > c) {
+    console.log("A is bigger Number")
+}
+else if (b > a && b > c) {
+    console.log("B is bigger Number")
+} else {
+    console.log("C is bigger Number");
+}

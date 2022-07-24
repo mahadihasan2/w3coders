dcf9782341a05f75e9bdc42ce138356b54e25951
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-25 03:52:46 +0600

    switch case using and check new day

--- class-5/switch-case.js
+++ class-5/switch-case.js
@@ -37,3 +37,32 @@ console.log(date);
 console.log(date.toDateString())
 console.log(date.toTimeString())
 console.log(date.toLocaleDateString())
+
+// Ajker din berKorar program 
+
+var day = date.getDay();
+switch (day) {
+    case 0:
+        console.log("This day is Sunday");
+        break
+    case 1:
+        console.log("This day is Monday");
+        break
+    case 2:
+        console.log("this day is TuiseDay");
+        break
+    case 3:
+        console.log("This day is wednesday");
+        break
+    case 4:
+        console.log("This day is Tusday");
+        break
+    case 5:
+        console.log("This day is friday");
+        break
+    case 6:
+        console.log("This day is Satarday");
+        break
+    default:
+        console.log("This day is not Match")
+}

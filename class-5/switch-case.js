f22a92b86a146703d1d26a40a1419016988506b2
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-25 03:32:01 +0600

    switch-case problem

---
+++ class-5/switch-case.js
@@ -0,0 +1,18 @@
+
+// switch-Case Practice 
+
+var color = "red";
+switch (color) {
+    case "green":
+        console.log("My Favorite Color is Green");
+        break
+    case "red":
+        console.log("My Favorite color is RED");
+        break
+    case "white":
+        console.log("My favorite color is White");
+        break
+    default:
+        console.log("My Favorite is not Matching")
+
+}

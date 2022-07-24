7c8c755340bb41595706135ddc6003b2516ccfbe
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-25 03:19:21 +0600

    update this code

--- class-5/leapyear.js
+++ class-5/leapyear.js
@@ -1,6 +1,6 @@
 
 // Check LeapYear for Using TernaryOperator
 
-var year = 2050;
-var YearResult = ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) ? "This year is Leap Year " : "This Year is not Leap Year";
+var year = 2054;
+var YearResult = ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) ? (year + "This year is Leap Year ") : (year + " " + "This Year is not Leap Year");
 console.log(YearResult);

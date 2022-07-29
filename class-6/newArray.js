407a826ef2368e1df47494e1f3a2a3f9c4789380
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-29 19:58:24 +0600

    newArray create

---
+++ class-6/newArray.js
@@ -0,0 +1,8 @@
+const studentName = ["Ariful", "Shohsn", "Kobir", "Kibriya"];
+const newArray = Array.from(studentName);
+console.log(newArray);
+newArray[0] = "Torikul"
+newArray[1] = "Hasibul"
+newArray[2] = "Shanto"
+newArray[3] = "Pranto"
+console.log(newArray);

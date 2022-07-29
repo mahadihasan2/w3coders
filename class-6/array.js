43daf1159d65e96615d593b297e88f0ba9df23f9
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-29 17:30:02 +0600

    array is start

---
+++ class-6/array.js
@@ -0,0 +1,16 @@
+var students = "Mahadi, Rakib, Shakib, Jibon";
+
+var studentsName = Array("Limon", "Shohan", "Tanvir", "Ibrahim");
+
+//Array Declare of 2nd type
+
+var studentId = [54, 43, 32, 21, 90, 56];
+
+console.log(studentsName);
+console.log(students);
+console.log(studentId);
+
+// Cheack the array up variable 
+console.log(Array.isArray(studentId));
+console.log(Array.isArray(students));
+console.log(Array.isArray(studentsName));

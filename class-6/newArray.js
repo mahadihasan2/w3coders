06f8b63a82e1dc3552099a38e91c322979dc3cd6
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-29 20:11:49 +0600

    push and pop method done

--- class-6/newArray.js
+++ class-6/newArray.js
@@ -6,3 +6,13 @@ newArray[1] = "Hasibul"
 newArray[2] = "Shanto"
 newArray[3] = "Pranto"
 console.log(newArray);
+
+// Push is the Array Method 
+const hereMe = [890, 43, 21, 65, 90];
+hereMe.push("W3Codevs is a best institute of programming institute");
+console.log(hereMe);
+
+// pop Method 
+const number = [54, 78, 89, 06, 40, 32];
+number.pop()
+console.log(number);

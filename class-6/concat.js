b7d548f0491b03c74ac7c5fd89cc2d1221a08da6
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-29 19:50:15 +0600

    concat done

---
+++ class-6/concat.js
@@ -0,0 +1,10 @@
+const studentsName = ["Limon", "Tareack", "Tomal", "Sabbir"];
+const studentId = [01, 02, 03, 54, 67, 89];
+const concat = studentsName.concat(studentId);
+console.log(concat);
+
+// Another to the concat an array method 
+const education = ["Borochapa", "Chadpur", "Chittagong", "Narsingdi"];
+const union = ["Borochapa", "Chadpur Chondona", "BodarHat", "Narsing Sodor"];
+const concatination = education.concat(union);
+console.log(concatination);

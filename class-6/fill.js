6b5fc53f707189f0fabb58fca2d0892744033a08
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-29 19:38:39 +0600

    array fill method is ok

---
+++ class-6/fill.js
@@ -0,0 +1,8 @@
+const std = ["Ahamed", "Jobadidul", "Ahona", "Afrin", "Mahadi"]
+const fill = std.fill("Khairul");
+console.log(fill);
+
+// Number check the fill Method 
+const number = [54, 43, 32, 89, 90, 21];
+const fills = number.fill(1000);
+console.log(fills);
